eb0d091e793f3356ba22e8e590c51eb8d6eca6a0
justinepeterson <jmahinyila@gmail.com>
2019-09-14 15:10:29 +0300

    Database connected

--- index.js
+++ index.js
@@ -1,15 +1,17 @@
 const express = require('express')
 const authRoutes = require('./routes/auth')
 const mongoose = require('mongoose')
+const dotenv = require('dotenv')
 
 const app = express();
+dotenv.config();
 
-mongoose.connect('mongodb://127.0.0.1:27017/auth',{ useNewUrlParser: true ,useUnifiedTopology: true })
+
+mongoose.connect(process.env.DB_CONNECT,{ useNewUrlParser: true ,useUnifiedTopology: true })
         .then(()=>console.log('Database connected'))
         .catch(err=>console.log(`Error: ${err}`))
 
 app.use('/api/user/',authRoutes)
-
 
 const PORT = process.env.PORT || 5000
 app.listen(PORT,()=>console.log(`App running on port ${PORT}`))
